a053be4f768f50079c3a84d813f790c243539ea7
veronica017 <u3532975@connect.hku.hk>
2019-04-01 23:06:09 +0800

    new file:   Assets/Art/Corridor.meta 	new file:   Assets/Art/Corridor/bat.png 	new file:   Assets/Art/Corridor/bat.png.meta 	new file:   Assets/Art/Corridor/carpet.png 	new file:   Assets/Art/Corridor/carpet.png.meta 	new file:   Assets/Art/Corridor/paint.png 	new file:   Assets/Art/Corridor/paint.png.meta 	new file:   Assets/Art/Corridor/wall.png 	new file:   Assets/Art/Corridor/wall.png.meta 	new file:   Assets/Scenes/Corridor.unity 	new file:   Assets/Scenes/Corridor.unity.meta 	new file:   Assets/Scripts/Bat.cs 	new file:   Assets/Scripts/Bat.cs.meta

---
+++ Assets/Scripts/Bat.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+/// <summary>
+/// This makes an object move randomly in a set of directions, with some random time delay in between each decision
+/// </summary>
+public class Bat : MonoBehaviour
+{
+    internal Transform thisTransform;
+ 
+    // The movement speed of the object
+    public float moveSpeed = 0.3f;
+ 
+    // A minimum and maximum time delay for taking a decision, choosing a direction to move in
+    public Vector2 decisionTime = new Vector2(1, 4);
+    internal float decisionTimeCount = 0;
+ 
+    // The possible directions that the object can move int, right, left, up, down, and zero for staying in place. I added zero twice to give a bigger chance if it happening than other directions
+    internal Vector3[] moveDirections = new Vector3[] { Vector3.right, Vector3.left, Vector3.up, Vector3.down, Vector3.zero, Vector3.zero };
+    internal int currentMoveDirection;
+ 
+    // Use this for initialization
+    void Start()
+    {
+        // Cache the transform for quicker access
+        thisTransform = this.transform;
+ 
+        // Set a random time delay for taking a decision ( changing direction, or standing in place for a while )
+        decisionTimeCount = Random.Range(decisionTime.x, decisionTime.y);
+ 
+        // Choose a movement direction, or stay in place
+        ChooseMoveDirection();
+    }
+ 
+    // Update is called once per frame
+    void Update()
+    {
+        // Move the object in the chosen direction at the set speed
+        thisTransform.position += moveDirections[currentMoveDirection] * Time.deltaTime * moveSpeed;
+ 
+        if (decisionTimeCount > 0) decisionTimeCount -= Time.deltaTime;
+        else
+        {
+            // Choose a random time delay for taking a decision ( changing direction, or standing in place for a while )
+            decisionTimeCount = Random.Range(decisionTime.x, decisionTime.y);
+ 
+            // Choose a movement direction, or stay in place
+            ChooseMoveDirection();
+        }
+        if(transform.position.y<-3/2)
+        {
+            thisTransform.position=new Vector3(0,-1/2);
+        }
+        if(transform.position.y>0)
+        {
+            thisTransform.position=new Vector3(0,-1/2);
+        }
+       
+    }
+ 
+    void ChooseMoveDirection()
+    {
+        // Choose whether to move sideways or up/down
+        currentMoveDirection = Mathf.FloorToInt(Random.Range(0, moveDirections.Length));
+    }
+
+    void OnTriggerEnter2D(Collider2D obj){
+        Debug.Log(1);
+    }
+}
+ 
