ba39a81efdf17f78a1ada63f81f9de9995859f3c
YangYafei1998 <faye.yangyafei@gmail.com>
2019-04-19 13:09:49 +0800

    make key collectable

---
+++ Assets/Scripts/KeyUsing.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyUsing : MonoBehaviour
+{
+    private Transform player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectsWithTag("Player")[0].transform;
+    }
+
+    // Update is called once per frame
+    public void Use()
+    {
+        Debug.Log("key is used");
+        Destroy(gameObject);
+    }
+}
