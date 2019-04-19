15fd905fcbae3ec0bd41f63882a9efbabd1c65cc
YangYafei1998 <faye.yangyafei@gmail.com>
2019-04-19 20:11:26 +0800

    no message

---
+++ Assets/Scripts/BoneUsing.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoneUsing : MonoBehaviour
+{
+    private Transform player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectsWithTag("Player")[0].transform;
+    }
+
+    // Update is called once per frame
+    public void Use()
+    {
+        Debug.Log("bone is used");
+        Destroy(gameObject);
+    }
+}
