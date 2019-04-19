f048935f2ed1be9a2ac43e8f63de356ac0f2d8e0
7D24H <31737338+7D24H@users.noreply.github.com>
2019-04-19 17:42:05 +0800

    modify hall left door

---
+++ Assets/Scripts/NPC.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    private Rigidbody2D myRigidbody;
+
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            print("You have to find a key");
+            print(myRigidbody.position);
+        }
+            
+    }
+
+}
