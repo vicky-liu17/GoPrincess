076556c22eb87508d8bd8f475855662a45d8802a
YangYafei1998 <faye.yangyafei@gmail.com>
2019-04-19 17:55:16 +0800

    no message

--- Assets/Dog.cs
+++ Assets/Dog.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class Dog : MonoBehaviour
 {
-    private Inventory inventory;
+    //private Inventory inventory;
     public GameObject itemButton;
     // Start is called before the first frame update
     void Start()
     {
-        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+        //inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
     }
 
     // Update is called once per frame
@@ -21,10 +21,6 @@ public class Dog : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log("meet dog");
-        //Transform transform = GameObject.FindGameObjectWithTag("dog_unseen").transform;
-        Transform transform = GameObject.FindGameObjectWithTag("Dog_thought_pos").transform;
 
-        //Instantiate(itemButton, transform, false);
-        Instantiate(itemButton, transform, false);
     }
 }
