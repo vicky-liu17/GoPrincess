15ece8108bcdcff0a6e7eb3540da1c467b37ea1e
veronica017 <u3532975@connect.hku.hk>
2019-04-19 17:10:05 +0800

    add a door in chamber

---
+++ Assets/Shadow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shadow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.SetActive(true);  
+       // Debug.Log(Time.timeSinceLevelLoad);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(Time.timeSinceLevelLoad >=2 && Time.timeSinceLevelLoad<=3)
+            gameObject.SetActive(false);  
+        
+       
+            
+    }
+}
